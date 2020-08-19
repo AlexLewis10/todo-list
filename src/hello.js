c6b59c68dd168cb42704d9d56d815bc4bd708620
Alex Lewis <alexlewis374@gmail.com>
2020-08-19 11:24:21 +0100

    G: Remove wash dishes from the todo array and return

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,9 @@
 export default class Todo {
 
   updateToDo (command) {
+    if (command === "Done 1") {
+      return "You have no to dos"
+    } 
     return [command]
   }
 }
--- test/hello.test.js
+++ test/hello.test.js
@@ -7,7 +7,13 @@ beforeEach(() => {
 })
 
 describe('Add an item to the todo list', () => {
-  it ('add wash dishes to todo array', () => {
+  it('add wash dishes to todo array', () => {
     expect(todo.updateToDo("Add wash dishes")).toStrictEqual(["Add wash dishes"])
+  })
+})
+
+describe('Remove an item from the todo list', () => {
+  it('remove wash dishes from the todo array and return', () => {
+    expect(todo.updateToDo("Done 1")).toStrictEqual("You have no to dos")
   })
 })
